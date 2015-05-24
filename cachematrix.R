93c762d8cea00e7ffd9359aa0c720e714c78f47c
MartinKData <MartinKData@gmx.de>
2015-05-24 23:43:47 +0200

    two functions makeCacheMatrix and cacheSolve added including comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The purpose of the two funtions makeCacheMatrix and cacheSolve is to take an
+## invertible matrix and compute its inverse matrix. If the inverse has already
+## been computed, it gets restored from the cache.
 
-## Write a short comment describing this function
+## makeCacheMatrix stores a matrix ma and returns a list of simple functions for
+## getting and setting the matrix and its inverse matrix.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(ma = matrix()) {
+    
+    inverseMatrix <- NULL
+    
+    set <- function(y) {
+        ma <<- y
+        inverseMatrix <<- NULL
+    }
+    
+    get <- function() ma
+    
+    setInverseMatrix <- function(solve) inverseMatrix <<- solve
+    
+    getInverseMatrix <- function() inverseMatrix
+    
+    list(set = set, get = get,
+         setInverseMatrix = setInverseMatrix,
+         getInverseMatrix = getInverseMatrix)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns the inverse matrix corresponding to a matrix object
+## created via the makeCacheMatrix function. If the inverse matrix has already
+## been calculated, cacheSolve returns the inverse from the cache. Else the
+## inverse will be calculated and cached with the matrix object.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(ma, ...) {
+    
+    inverseMatrix <- ma$getInverseMatrix()
+    
+    if(!is.null(inverseMatrix)) {
+        message("getting cached inverse matrix")
+        return(inverseMatrix)
+    }
+    
+    matData <- ma$get()
+    dimMatData <- dim(matData)[1]
+    inverseMatrix <- solve(matData, diag(dimMatData), ...)
+    ma$setInverseMatrix(inverseMatrix)
+    
+    inverseMatrix
 }
